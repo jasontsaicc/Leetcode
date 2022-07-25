3c6814c8e002897ab3b9bfa221cc50910cf0de22
jason tsai <jason.tsaicc@gmail.com>
2022-07-25 14:42:27 +0800

    14. Longest Common Prefix

--- 一. 數組/14. Longest Common Prefix.py
+++ 一. 數組/14. Longest Common Prefix.py
@@ -1,3 +1,40 @@
+"""
+Write a function to find the longest common prefix string amongst an array of
+strings.
+
+ If there is no common prefix, return an empty string "".
+
+
+ Example 1:
+
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+
+ Example 2:
+
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+
+
+ Constraints:
+
+
+ 1 <= strs.length <= 200
+ 0 <= strs[i].length <= 200
+ strs[i] consists of only lowercase English letters.
+
+
+ Related Topics String 👍 9199 👎 3211
+
+"""
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         s = ""
@@ -9,4 +46,10 @@ class Solution:
             else:
                 break
         return s
+
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+
+
 
