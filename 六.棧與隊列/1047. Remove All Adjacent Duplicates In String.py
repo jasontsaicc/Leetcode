53357bfd31053e0f7364c1f3e6fb35a3789037dc
jasontsaicc <jason.tsaicc@gmail.com>
2022-08-07 23:23:51 +0800

    Update 1047 多一種寫法

--- 六.棧與隊列/1047. Remove All Adjacent Duplicates In String.py
+++ 六.棧與隊列/1047. Remove All Adjacent Duplicates In String.py
@@ -1,5 +1,6 @@
 class Solution:
     def removeDuplicates(self, s):
+        # 方法一，使用棧，推薦！
         result = []
         for i in s:
             if result and result[-1] == i:
@@ -7,6 +8,22 @@ class Solution:
             else:
                 result.append(i)
         return "".join(result)
+    # 方法二，使用雙指針模擬棧，如果不讓用棧可以作為備選方法。
+        res = list(s)
+        slow = fast = 0
+        length = len(res)
+
+        while fast < length:
+            # 如果一樣直接換，不一樣會把後面的填在slow的位置
+            res[slow] = res[fast]
+            # 如果發現和前一個一樣，就退一格指針
+            if slow > 0 and res[slow] == res[slow - 1]:
+                slow -= 1
+            else:
+                slow += 1
+            fast += 1
+        return ''.join(res[0:slow])
+
 
 
 # s = Solution()
