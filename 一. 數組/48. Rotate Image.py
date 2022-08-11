b867f83ef8bb5090f8784448524181c1155fd859
jasontsaicc <jason.tsaicc@gmail.com>
2022-08-11 14:08:11 +0800

    48. Rotate Image

---
+++ 一. 數組/48. Rotate Image.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+
+        # 順時針反轉九十度可以分成兩步
+        1.先對角線翻轉
+        2.接著進行列方向的逆序
+
+        """
+
+
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
+
